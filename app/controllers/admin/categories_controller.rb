0b804f6321ee7ba9dfa2884453f7a21a9b363ea7
vuongsythanh <vuong.sy.thanh@framgia.com>
2017-03-02 14:29:46 +0700

    fix create categories - version finally

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,28 @@
 class Admin::CategoriesController < ApplicationController
   def index
-    @categories = Category.select(:id, :name).order(:name).includes(:books)
-      .page(params[:page]).per(Settings.categories.categories_per_page)
+    @categories = Category.select(:id, :name).order(created_at: :desc)
+      .includes(:books).page(params[:page])
+      .per Settings.categories.per_page
+    @category = Category.new
+  end
+
+  def new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] = t ".create_cat"
+      redirect_to admin_categories_url
+    else
+      flash[:danger] = t ".not_create_cat"
+      redirect_to admin_categories_url
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit :name, :description
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,3 @@
   description = Faker::Lorem.paragraph
   Category.create! name: name, description: description
 end
-
-40.times do |n|
-  title = Faker::Name.title
-  description = Faker::Lorem.paragraph
-  status = true
-  category_id = 1 + (rand 40)
-  Book.create! title: title, description: description, status: status, category_id: category_id
-end
