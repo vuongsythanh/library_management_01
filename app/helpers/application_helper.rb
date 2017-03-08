0168c7458fb604c07dc1839bb515d868eb9161d4
vuongsythanh <vuong.sy.thanh@framgia.com>
2017-03-08 13:21:29 +0700

    update categories
    search categories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     base_title = t ".app_name_app"
     page_title.empty? ? base_title : page_title +  " | " + base_title
   end
+
+  def index_continue object, index, per_page
+    (object.to_i - 1) * per_page.to_i + index + 1
+  end
 end
