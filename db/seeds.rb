aba1388095e7e496f673ef0a9726e29be0b2dd52
vuongsythanh <vuong.sy.thanh@framgia.com>
2017-07-03 13:45:21 +0700

    fix_home_pages-second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,3 +31,13 @@ end
   Book.create! title: title, description: description, status: status, image: image,
     category_id: category_id, author_id: author_id, publisher_id: publisher_id
 end
+
+60.times do |n|
+  publish_date = Faker::Date.between(2.days.ago, Date.today)
+  number_page = Random.rand(1..10)
+  limit_date = Random.rand(1..4)
+  description = Faker::Lorem.paragraph
+  book_id = Random.rand(1..60)
+  AttachBook.create! publish_date: publish_date, number_page: number_page,
+    limit_date: limit_date, description: description, book_id: book_id
+end
